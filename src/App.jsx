a7ba4bd954c9914525b07460aef26b1fda5c3b61
Anju <anju22bai71319@gmail.com>
2025-10-20 14:08:28 +0530

    changes done

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import './css/App.css'
+import MovieCard from './components/MovieCard';
+import Favorites from './pages/Favorites';
+import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import { MovieProvider } from './contexts/MovieContext';
+function App() {
+ 
+
+  return (
+    <MovieProvider>
+      <NavBar/>
+    <main className="main-content"><Routes>
+    <Route path='/'element={<Home />}></Route>
+    <Route path='/Favorites'element={<Favorites />}></Route>
+    
+    </Routes>
+        </main>
+    
+</MovieProvider>
+
+  );
+}
+
+export default App;
+ 
